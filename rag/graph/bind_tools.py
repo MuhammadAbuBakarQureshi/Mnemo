7cbc71c10c976d4de526f817697aee3ed62ce2dd
Bakar Qureshi <qureshimuhammadabubakar@gmail.com>
2026-07-04 10:31:00 +0500

    Create bind_tools.py

---
+++ rag/graph/bind_tools.py
@@ -0,0 +1,26 @@
+from langchain_groq import ChatGroq
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from rag.vector_store.query_db import make_retrieve_tool
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+try:
+
+    def build_llm_with_tools(db: AsyncSession) -> ChatGroq:
+
+        llm_name = "llama-3.3-70b-versatile"
+        llm = ChatGroq(model=llm_name)
+
+        retrieve_tool = make_retrieve_tool(db)          # db captured in closure
+        llm_with_tools = llm.bind_tools([retrieve_tool])
+
+        return llm_with_tools, [retrieve_tool]
+    
+
+except Exception as e:
+
+    print(f"Building LLM with tools Error: {e}")
