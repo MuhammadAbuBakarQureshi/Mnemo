0d43dea6dccd9c44dbc7a92ba7e42591f83f2ecd
Bakar Qureshi <qureshimuhammadabubakar@gmail.com>
2026-07-04 10:32:35 +0500

    Create auth.py

---
+++ backend/app/dependencies/auth.py
@@ -0,0 +1,35 @@
+from fastapi import Depends, HTTPException, Request
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from backend.app.core.database import db_session_maker
+from backend.app.models.models import User
+from backend.app.auth.jwt import decode_access_token # type: ignore
+
+
+async def get_current_user(
+    request: Request,
+    session: AsyncSession = Depends(db_session_maker)
+) -> User:
+
+    token = request.cookies.get("access_token")
+
+    if not token:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+
+    try:
+        payload = decode_access_token(token)
+    except Exception:
+        raise HTTPException(status_code=401, detail="Invalid or expired session")
+
+    user_id = payload.get("sub")
+    if user_id is None:
+        raise HTTPException(status_code=401, detail="Invalid token payload")
+
+    result = await session.execute(select(User).where(User.user_id == int(user_id)))
+    user = result.scalar_one_or_none()
+
+    if user is None:
+        raise HTTPException(status_code=401, detail="User no longer exists")
+
+    return user
