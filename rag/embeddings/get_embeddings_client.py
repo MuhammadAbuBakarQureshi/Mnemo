2255511c4bdf17b3923696a5759048a4df3940f7
Bakar Qureshi <qureshimuhammadabubakar@gmail.com>
2026-07-04 11:47:05 +0500

    Update get_embeddings_client.py

--- rag/embeddings/get_embeddings_client.py
+++ rag/embeddings/get_embeddings_client.py
@@ -1,11 +1,14 @@
 import os
 from langchain_ollama import OllamaEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_embeddings_client():
 
     embeddings_client = OllamaEmbeddings(
         model="nomic-embed-text:latest",
-        base_url=os.getenv("OLLAMA_BASE_URL", "http://localhost:11434")
+        base_url=os.getenv("OLLAMA_BASE_URL", os.getenv("OLLAMA_URL"))
     )
 
     return embeddings_client
