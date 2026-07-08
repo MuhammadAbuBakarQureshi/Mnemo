2a967fa5775d36708acf2d8ddc0f6c598d963a9d
Bakar Qureshi <qureshimuhammadabubakar@gmail.com>
2026-07-08 19:34:21 +0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from backend.app.api.v1.projects import router as project_router
 from backend.app.api.v1.users import router as user_router
-from backend.app.api.v1.auth import router as auth_router
 from backend.app.api.v1.chats import router as chat_router
 from backend.app.api.v1.documents import router as document_router
 
@@ -13,7 +12,6 @@ app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
 app.include_router(router=project_router, prefix="/project")
 app.include_router(router=user_router, prefix="/user")
-app.include_router(router=auth_router, prefix="/auth")
 app.include_router(router=chat_router, prefix="/chat")
 app.include_router(router=document_router, prefix="/documents")
 
